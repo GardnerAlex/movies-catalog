9a2c33657aeb4fef0c08556e0cfe9a0423f84e85
Гарднер Александр Анатольевич <alexander.gardner.g@gmail.com>
2020-09-03 18:32:44 +0300

    materialUIv1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,46 @@
+// @ts-ignore
 import React, { useEffect, useState } from 'react';
 import { fetchMovies } from '../../api/api';
 import { ImoviesData } from '../../interfaces/interfaces';
-import Movie from '../Movie/Movie';
+
+import { Movie, RecipeReviewCard } from '../Movie/Movie';
 import './App.css';
 
-export const App = () => {
-    const [moviesData, setSignUpData] = useState<ImoviesData[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState<string>(null);
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(4),
+      textAlign: 'right',
+      color: theme.palette.text.secondary,
+      width: '350px'
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }),
+);
+
+export const App = () => {
+  const classes = useStyles();
+  const [moviesData, setSignUpData] = useState<ImoviesData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>(null);
 
   useEffect(() => {
-      setLoading(true);
-        fetchMovies()
-            .then(jsonResponse => {
-                setLoading(false);
+    setLoading(true);
+    fetchMovies()
+      .then(jsonResponse => {
+        setLoading(false);
         console.log(jsonResponse);
         setSignUpData(jsonResponse.Search);
       })
-          .catch(err => setErrorMessage(err.toString()))
+      .catch(err => setErrorMessage(err.toString()));
   }, []);
 
   return (
@@ -32,11 +54,26 @@ export const App = () => {
         {errorMessage
         && <span>{errorMessage}</span>}
 
-        {moviesData
-        && moviesData.map((movie, index) => (
-          <Movie key={`${movie.imdbIDTitle}${index}`} movie={movie} />
-        ))}
-
+        {/*{moviesData*/}
+        {/*&& moviesData.map((movie, index) => (*/}
+        {/*  <Movie key={`${movie.imdbIDTitle}${index}`} movie={movie} />*/}
+        {/*))}*/}
+        <Grid container className={classes.root} spacing={3}>
+          <Grid item xs={12}>
+            <Grid container justify="center" spacing={2}>
+              {moviesData
+              && moviesData.map((movie, index) => (
+                <Grid className={classes.paper} key={`${movie.imdbIDTitle}${index}`} item>
+                    <RecipeReviewCard inputData={movie} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+        {/*{moviesData*/}
+        {/*&& moviesData.map((movie, index) => (*/}
+        {/*  <RecipeReviewCard key={`${movie.imdbIDTitle}${index}`} inputData={movie} />*/}
+        {/*))}*/}
       </div>
     </div>
   );
--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -15,6 +15,8 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import { ImoviesData } from '../../interfaces/interfaces';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -41,7 +43,7 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const Movie = ({ movie }) => {
+export const Movie = ({ movie }) => {
   return (
     <figure className="card">
       <img
@@ -53,4 +55,81 @@ const Movie = ({ movie }) => {
   );
 };
 
-export default Movie;
+export const RecipeReviewCard = ({ inputData }) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+  const [favorites, setFavorites] = useState('primary');
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleFavoritesClick = () => {
+    if (favorites === 'primary') {
+      setFavorites('secondary');
+    } else {
+      setFavorites('primary');
+    }
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={(
+          <Avatar aria-label="movie" className={classes.avatar}>
+            R
+          </Avatar>
+        )}
+        action={(
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        )}
+        title={inputData.Title}
+        subheader={`${inputData.Type} ${inputData.Year}`}
+      />
+      <CardMedia
+        className={classes.media}
+        image={inputData.Poster}
+        title={inputData.Title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {inputData.Title}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton
+          aria-label="add to favorites"
+          onClick={handleFavoritesClick}
+          color={favorites}
+        >
+          <FavoriteIcon
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Details:</Typography>
+          <Typography paragraph>
+            Can be description:
+            {inputData.Title}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
