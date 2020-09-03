c06cab97a29e6760bb2263d2910ef1857970668a
Гарднер Александр Анатольевич <alexander.gardner.g@gmail.com>
2020-09-03 15:42:56 +0300

    hooks 1st version

--- src/api/api.js
+++
@@ -1,6 +0,0 @@
-const MOVIE_API_URL = 'https://www.omdbapi.com/?apikey=819878a1';
-
-export const fetchMovies = (search = 'war') => (
-  fetch(`${MOVIE_API_URL}&s=${search}`)
-    .then(response => response.json())
-);
